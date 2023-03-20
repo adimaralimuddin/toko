9e5239aab4b4c65f8c79439ce20c39cb38d2686d
adimar <adimaraisha@gmail.com>
2023-03-20 12:11:36 +0800

    updated environment variable for production

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
   env: {
     // MONGO_URL:
     //   "mongodb+srv://adimar:adimar123@cluster0.rxkel.mongodb.net/?retryWrites=true&w=majority",
-    MONGO_URL:
-      "mongodb://adimar:adimar123@cluster0-shard-00-00.rxkel.mongodb.net:27017,cluster0-shard-00-01.rxkel.mongodb.net:27017,cluster0-shard-00-02.rxkel.mongodb.net:27017/?ssl=true&replicaSet=atlas-uis4j8-shard-0&authSource=admin&retryWrites=true&w=majority",
+
+    MONGO_URL: process.env.MONGO_URL,
     // MONGO_URL: "mongodb://localhost:27017/food-app-1",
     Base_URL: process.env.NEXT_PUBLIC_BASE_URL,
     // Base_URL: "http://localhost:3000",
