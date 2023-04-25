7b8cea0d991135b7853578ab927cd501eaa1eb9f
adimar <adimaraisha@gmail.com>
2023-04-25 17:26:53 +0800

    change mongodb url

--- utils/connectMongoose.js
+++ utils/connectMongoose.js
@@ -1,8 +1,10 @@
 const { default: mongoose } = require("mongoose");
 
 // const MONGO_URL = process.env.MONGO_URL;
+// const MONGO_URL =
+//   "mongodb://adimar:adimar123@cluster0-shard-00-00.rxkel.mongodb.net:27017,cluster0-shard-00-01.rxkel.mongodb.net:27017,cluster0-shard-00-02.rxkel.mongodb.net:27017/?ssl=true&replicaSet=atlas-uis4j8-shard-0&authSource=admin&retryWrites=true&w=majority";
 const MONGO_URL =
-  "mongodb://adimar:adimar123@cluster0-shard-00-00.rxkel.mongodb.net:27017,cluster0-shard-00-01.rxkel.mongodb.net:27017,cluster0-shard-00-02.rxkel.mongodb.net:27017/?ssl=true&replicaSet=atlas-uis4j8-shard-0&authSource=admin&retryWrites=true&w=majority";
+  "mongodb+srv://adimar:adimar123@cluster0.rxkel.mongodb.net/?retryWrites=true&w=majority";
 
 console.log("MOngo-url here", MONGO_URL);
 
